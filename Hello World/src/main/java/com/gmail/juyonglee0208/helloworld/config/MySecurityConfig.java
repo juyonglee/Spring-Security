6fca69c439843abe3281d6720123492f06b38052
juyonglee <juyonglee0208@gmail.com>
2023-02-05 11:49:43 +0900

    /auth 접근 권한 수정

--- Hello World/src/main/java/com/gmail/juyonglee0208/helloworld/config/MySecurityConfig.java
+++ Hello World/src/main/java/com/gmail/juyonglee0208/helloworld/config/MySecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MySecurityConfig {
 
@@ -34,7 +36,7 @@ public class MySecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((request) -> request
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/auth").permitAll()
                 .anyRequest().authenticated()
             );
         http.formLogin();
