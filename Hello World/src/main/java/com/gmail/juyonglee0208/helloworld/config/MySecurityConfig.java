aa6cad20b2aa08d96fc7d68ab76043edd3caac90
juyonglee <juyonglee0208@gmail.com>
2023-02-05 10:36:57 +0900

    PasswordEncoder 기능 구현

--- Hello World/src/main/java/com/gmail/juyonglee0208/helloworld/config/MySecurityConfig.java
+++ Hello World/src/main/java/com/gmail/juyonglee0208/helloworld/config/MySecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -18,5 +20,11 @@ public class MySecurityConfig {
         UserDetails admin = User.withUsername("admin").password(passwordEncoder().encode("0000"))
             .roles("ADMIN").build();
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
