ebc9856ad2171611241e6bdc35339e45dd63c00f
juyonglee <juyonglee0208@gmail.com>
2023-02-05 10:41:02 +0900

    SecurityFilterChain 기능 구현

--- Hello World/src/main/java/com/gmail/juyonglee0208/helloworld/config/MySecurityConfig.java
+++ Hello World/src/main/java/com/gmail/juyonglee0208/helloworld/config/MySecurityConfig.java
@@ -3,11 +3,13 @@ package com.gmail.juyonglee0208.helloworld.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -26,5 +28,17 @@ public class MySecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((request) -> request
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+            );
+        http.formLogin();
+        http.httpBasic();
+        return http.build();
     }
 }
