1c5eb6eaf2c4442a5e342c3734a6dc44582cd044
juyonglee <juyonglee0208@gmail.com>
2023-02-14 14:30:10 +0900

    Basic Login Project Init

---
+++ Basic Login/src/main/java/com/gmail/juyonglee0208/basic_login/BasicLoginApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.juyonglee0208.basic_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicLoginApplication.class, args);
+    }
+
+}
---
+++ Basic Login/src/test/java/com/gmail/juyonglee0208/basic_login/BasicLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.juyonglee0208.basic_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
